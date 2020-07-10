6caf4f70a65569dc62a0b1abaf7cd266dee35cfc
高橋良輔 <r-takahashi@image-pit.com>
2020-07-10 10:37:11 +0900

    独自例外を追加

--- java-oop/src/text/SameException.java
+++ java-oop/src/text/SameException.java
@@ -1,7 +1,21 @@
 package text;
 
+/**
+ * 独自例外クラス
+ * @author imagepit
+ *
+ */
 public class SameException extends Exception{
+	// キャッチした場合にメッセージなどを送りたい場合は
+	// 親クラスのコンストラクタを再定義する
 	public SameException(String message) {
-		super(message);
+		super(message); // 親クラスのコンストラクタを呼び出す
+		// そうするとcatchしたときにgetMessageで取得できる
+	}
+	// Throwableを引数にすると
+	// （後述）例外をラップしてさらに呼び出し元に例外を投げるときに
+	// その経緯をより詳細にprintStackTraceに出力できる
+	public SameException(String message, Throwable e) {
+		super(message,e);
 	}
 }
