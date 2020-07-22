09ddb50954156786fdb03fec2ed50c4d107ba058
高橋良輔 <r-takahashi@image-pit.com>
2020-07-22 14:05:02 +0900

    フィルターのサンプルコード

---
+++ web/src/text/ServletFilter.java
@@ -0,0 +1,31 @@
+package text;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+// ↓すべてのURLパターンのリクエストに対してこのクラスのフィルターを通す
+@WebFilter("/*")
+public class ServletFilter implements Filter{
+
+	@Override
+	public void doFilter(
+			ServletRequest request,
+			ServletResponse response,
+			FilterChain chain
+	)throws IOException, ServletException {
+		// 前処理
+		System.out.println("前処理");
+		request.setCharacterEncoding("UTF-8");
+		// Servletのメイン処理が実行される
+		chain.doFilter(request, response);
+		// 後処理
+		System.out.println("後処理");
+	}
+
+}
