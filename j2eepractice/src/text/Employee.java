593dd014f77c2a23ba118a4e03ee059b77859e2a
高橋良輔 <r-takahashi@image-pit.com>
2020-07-27 12:57:15 +0900

    Employeeクラス追加

---
+++ j2eepractice/src/text/Employee.java
@@ -0,0 +1,14 @@
+package text;
+
+public class Employee {
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
